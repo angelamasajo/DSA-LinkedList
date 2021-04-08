39107a39faf3ac5ee8b3d3f6868b69947d80939a
Angela Masajo <aabmasajo@gmail.com>
2021-04-08 00:36:03 -0500

    assignments added

--- Assignment/LinkedList.js
+++ Assignment/LinkedList.js
@@ -22,6 +22,34 @@ class LinkedList {
     }
   }
 
+  insertBefore(insertItem, value) {
+    let currNode = this.head
+    while(value !== currNode.next.value) {
+      currNode = currNode.next
+    }
+    let findValue = this.find(value)
+
+    currNode.next = new _Node(insertItem, findValue)
+  }
+
+  insertAfter(insertItem, value) {
+    let findValue = this.find(value)
+    let tempNext = findValue.next
+    findValue.next = new _Node(insertItem, tempNext)
+  }
+
+  insertAt(insertItem, position) {
+    let currNode = this.head
+    let count = 0
+    while (currNode.next !== null) {
+      count++
+      if (count === position) {
+        this.insertBefore(insertItem, currNode.value)
+      }
+      currNode = currNode.next
+    }
+  }
+
   find(item) {
     let currNode = this.head;
     if (!this.head) {
@@ -60,6 +88,147 @@ class LinkedList {
     previousNode.next = currNode.next
   } 
 
+}
+
+reverseList = () => {
+  let currNode = this.head; // start at the beginning
+  let node = null; // save the previous thing to point at
+  while (currNode!== null) { //[1, 2, 3, 4]
+    let tempNext = currNode.next; //2 //3
+    currNode.next = node;//1 -> null //3
+    node = currNode; //value from null to 1
+    currNode = tempNext; //value from 1 to 2
+  }
+  this.head = node;
+}
+
+reverseListRecursive = (node) => {
+    // if 0 node in list
+    if (node == null) {
+        return null;
+    }
+    // if 1 node in list
+    if (node.next == null) {
+        return node;
+    }
+    // reverse recursively and link second.next to first 
+    const secondElem = node.next;
+    node.next = null;
+    const reverseRest = reverseList(secondElem);
+    secondElem.next = node;
+    return reverseRest;
+}
+
+reverseListIterative = (lst) => {
+  // create the list by adding to reversedPart
+  let reversedPart = null;
+  let current = lst.head;
+ 
+  while(current !== null) {
+    // assign next node to savedNode for safe keeping
+    let savedNode = current.next;
+    // Reassigns current's pointer to the new list we are making
+    // reversedPart starts as null, but slowly grows as things are
+    // pushed onto the new list
+    current.next = reversedPart;
+    reversedPart = current;
+    // update the counter that is traveling through the array
+    current = savedNode;
+  }
+  // update the head so we have access to the linked list
+  lst.head = reversedPart;
+  //displayList(lst);
+  return lst;
+}
+
+
+
+display = () => {
+  let currNode = this.head
+  while (currNode !== null) {
+    console.log(currNode.value)
+    currNode = currNode.next
+  }
+}
+
+size = () => {
+  let currNode = this.head
+  let counter = 0
+  while (currNode !== null) {
+    counter++
+    currNode = currNode.next
+  }
+  console.log(counter)
+  return counter
+}
+
+isEmpty = () => {
+  let currNode = this.head
+  if (currNode === null) {
+    return true
+  }
+  return false
+}
+
+findPrevious = (item) => {
+  let currNode = this.head
+  while (item !==  currNode.next.value) {
+    currNode = currNode.next
+  }
+  return currNode
+}
+
+findLast = () => {
+  let currNode = this.head
+  while (currNode.next !== null) {
+    currNode = currNode.next
+  }
+  return currNode
+}
+
+thirdFromEnd = () => {
+  let currNode = this.head
+  while (currNode.next.next.next !== null) {
+    currNode = currNode.next
+  }
+  console.log('third fom end', currNode.value)
+  return currNode
+}
+
+middleOfList = () => {
+  const listLength = this.size()
+  let currNode = this.head
+  let position = 0
+  let count = 0
+  if (listLength % 2 === 1) {
+    position = listLength / 2 + 0.5
+  } 
+  else {
+    position = listLength / 2
+  }
+
+  while (currNode.next !== null) {
+    count++;
+    if (count === position) {
+      return currNode.value
+    }
+    currNode = currNode.next
+  }
+}
+
+cycleInAList = () => {
+  let currNode = this.head;
+  let values = [];
+
+  while (currNode !== null) {
+    if (values.includes(currNode.value)) {
+      return true;
+    }
+    values.push(currNode.value);
+    currNode = currNode.next;
+  }
+
+  return false;
 }
 
 module.exports = LinkedList
--- Assignment/index.js
+++ Assignment/index.js
@@ -5,7 +5,17 @@ const SLL = new LinkedList();
 function main() {
   SLL.insertFirst('Apollo');
   SLL.insertFirst('Boomer');
+  SLL.insertFirst('Helo');
+  SLL.insertFirst('Husker');
+  SLL.insertFirst('Starbuck');
 
+  SLL.remove('Husker');
+  //previous is now undefined
+
+  SLL.insertBefore('Athena', 'Boomer')
+  SLL.insertAfter('Hotdog', 'Helo')
+  SLL.insertAt('Kat', 3)
+  console.log(SLL)
 }
 
-console.log(main())
+main()
